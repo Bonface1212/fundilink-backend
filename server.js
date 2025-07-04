64f27d16faed6e58603460d4d211d002ba97d943
Bonface1212 <mamboleobonface001@gmail.com>
2025-07-04 15:31:05 +0300

    password apdt

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ if (!fs.existsSync(uploadDir)) {
 }
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Password checker
